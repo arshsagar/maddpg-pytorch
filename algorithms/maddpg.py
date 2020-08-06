3bde6a92fee59884464d27181611dbda1ae97d7d
sagartwelve <arshdeepsingh79537@gmail.com>
2020-08-07 01:10:13 +0530

    changed lambda function

--- algorithms/maddpg.py
+++ algorithms/maddpg.py
@@ -235,30 +235,37 @@ class MADDPG(object):
         """
         Instantiate instance of this class from multi-agent environment
         """
+        discrete_action = False
+        flag_discrete = False
         agent_init_params = []
         alg_types = [adversary_alg if atype == 'adversary' else agent_alg for
                      atype in env.agent_types]
         for acsp, obsp, algtype in zip(env.action_space, env.observation_space,
                                        alg_types):
+            print('acsp :', acsp)
             num_in_pol = obsp.shape[0]
-            if isinstance(acsp, Box):
-                discrete_action = False
-                get_shape = lambda x: x.shape[0]
-            else:  # Discrete
-                discrete_action = True
-                get_shape = lambda x: x.n
+            def get_shape(x):
+                if isinstance(x, Box):
+                    return x.shape[0]
+                else:  # Discrete
+                    flag_discrete = True
+                    return x.n
             num_out_pol = get_shape(acsp)
             if algtype == "MADDPG":
                 num_in_critic = 0
                 for oobsp in env.observation_space:
                     num_in_critic += oobsp.shape[0]
                 for oacsp in env.action_space:
+                    print('oacsp :', oacsp)
                     num_in_critic += get_shape(oacsp)
             else:
                 num_in_critic = obsp.shape[0] + get_shape(acsp)
             agent_init_params.append({'num_in_pol': num_in_pol,
                                       'num_out_pol': num_out_pol,
                                       'num_in_critic': num_in_critic})
+        # end for
+        if flag_discrete == True:
+            discrete_action = True
         init_dict = {'gamma': gamma, 'tau': tau, 'lr': lr,
                      'hidden_dim': hidden_dim,
                      'alg_types': alg_types,
